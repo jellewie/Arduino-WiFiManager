5ae0bf78e848c506f97a4617acf4fcf31f6dfeb6
JelleWie <1545223+jellewie@users.noreply.github.com>
2020-09-24 15:28:38 +0200

    Updated to more lib version

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -5,22 +5,23 @@
 #include <WiFi.h>                   //we need this for wifi stuff (duh)
 #include <WebServer.h>
 WebServer server(80);
+#include "WifiManager.h"
 
 void setup() {
   Serial.begin(115200);
   server.on("/",          WiFiManager_handle_Connect);    //Must be declaired before "WiFiManager_Start()" for APMode
   server.on("/setup",     WiFiManager_handle_Settings);   //Must be declaired before "WiFiManager_Start()" for APMode
 
-  byte Answer = WiFiManager_Start();                      //run the wifi startup (and save results)
+  byte Answer = WiFiManager.Start();                      //run the wifi startup (and save results)
   
   Serial.println("setup done with code '" + String(Answer) + "'");
   
-  //WiFiManager_StartServer();      //Start the server (if you need this for other stuff)
-  //WiFiManager_EnableSetup(true);  //Enable the settings, only enabled in APmode by default
-  //WiFiManager_WriteEEPROM();      //Since we can't really check if the new input values work, this is just stored local and not in EEPROM
+  //WiFiManager.StartServer();      //Start the server (if you need this for other stuff)
+  //WiFiManager.EnableSetup(true);  //Enable the settings, only enabled in APmode by default
+  //WiFiManager.WriteEEPROM();      //Since we can't really check if the new input values work, this is just stored local and not in EEPROM
                                     //^use this to save values to (EEPROM LIMITED WRITES AVAILIBLE! do not spam)
 }
 void loop() {
 
-  //WiFiManager_RunServer();     //Do WIFI server stuff if needed. Only uncomment this if you need the server. You must have started the server before this with 'WiFiManager_StartServer()'
+  //WiFiManager.RunServer();     //Do WIFI server stuff if needed. Only uncomment this if you need the server. You must have started the server before this with 'WiFiManager_StartServer()'
 }
